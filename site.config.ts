3e50e193fc1e721bd5c83a075f5f756e5efc5998
Simeon Bowman <134252285+DOJO-MNKY@users.noreply.github.com>
2024-01-10 16:22:28 -0500

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,7 +9,7 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'MOOD MNKY BLOG',
+  name: 'JUPITERS GIRTH BLOG',
   domain: 'blog.moodmnky.com',
   author: 'Simeon Bowman',
 
